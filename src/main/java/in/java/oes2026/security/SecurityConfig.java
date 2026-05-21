0f40930029fcd749a09a06eb15be1023d3ae35ff
kuntald22232 <your-github-kuntaldas246567@gmail.com>
2026-05-21 09:32:21 +0530

    fixed cors issue

--- src/main/java/in/java/oes2026/security/SecurityConfig.java
+++ src/main/java/in/java/oes2026/security/SecurityConfig.java
@@ -3,13 +3,16 @@ package in.java.oes2026.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;*/
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -26,17 +29,14 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
-                        // Allow CORS preflight requests
                         .requestMatchers(
                                 HttpMethod.OPTIONS,
                                 "/**"
                         ).permitAll()
 
-                        // PUBLIC
                         .requestMatchers("/api/auth/**")
                         .permitAll()
 
-                        // EXAM APP (FULL OPEN)
                         .requestMatchers("/api/exams/**")
                         .permitAll()
 
@@ -49,7 +49,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/teacher/**")
                         .permitAll()
 
-                        // ROLE BASED
                         .requestMatchers("/api/student/**")
                         .hasRole("STUDENT")
 
@@ -70,6 +69,47 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of(
+                        "http://localhost:3000"
+                )
+        );
+
+        configuration.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+        );
+
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
+    }
+
+
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration config
@@ -77,10 +117,4 @@ public class SecurityConfig {
 
         return config.getAuthenticationManager();
     }
-
-    // IMPORTANT
-  /*  @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }*/
 }
