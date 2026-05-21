780672a0589ed8eeace704686c3019e2986542ca
kuntald22232 <your-github-kuntaldas246567@gmail.com>
2026-05-21 23:26:59 +0530

    fix render startup

--- src/main/java/in/java/oes2026/security/SecurityConfig.java
+++ src/main/java/in/java/oes2026/security/SecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;*/
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -117,10 +117,5 @@ public class SecurityConfig {
     ) throws Exception {
 
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    } 
 }
