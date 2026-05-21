ee464b5940ef3a9bb1a7f6eda72d86b90f077642
kuntald22232 <your-github-kuntaldas246567@gmail.com>
2026-05-21 09:01:49 +0530

    Fix duplicate password encoder bean

--- src/main/java/in/java/oes2026/security/SecurityConfig.java
+++ src/main/java/in/java/oes2026/security/SecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;*/
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -79,8 +79,8 @@ public class SecurityConfig {
     }
 
     // IMPORTANT
-    @Bean
+  /*  @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 }
