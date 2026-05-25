3d420905ae552308054102f310078099a8c04af8
kuntald22232 <your-github-kuntaldas246567@gmail.com>
2026-05-25 08:01:58 +0530

    Updated 7

--- src/main/java/in/java/oes2026/exam/submission/controller/TeacherSubmissionController.java
+++ src/main/java/in/java/oes2026/exam/submission/controller/TeacherSubmissionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/teacher")
+@RequestMapping("/api/examiner")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class TeacherSubmissionController {
